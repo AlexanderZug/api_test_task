349874f29dbff50cee7aa807f53c5e96df6fa2c7
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-08-13 17:34:32 +0300

    настройка пермишена

--- api/permissions.py
+++ api/permissions.py
@@ -1,11 +1,18 @@
 from rest_framework import permissions
 
 
-class OwnerOrReadOnly(permissions.BasePermission):
+class OwnerOrReadOnlyUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.username == request.user.username
+
+
+class OwnerOrReadOnlyTask(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user.username == request.user.username
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
--- api/views.py
+++ api/views.py
@@ -4,20 +4,22 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.models import Task, User
-from api.permissions import OwnerOrReadOnly, IsAdminOrReadOnly
+from api.permissions import OwnerOrReadOnlyUser, OwnerOrReadOnlyTask
 from api.serializers import TaskSerializer, UserSerializer
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (OwnerOrReadOnly,)
+    permission_classes = (OwnerOrReadOnlyUser,)
+    http_method_names = ['head', 'get', 'put', 'patch', 'delete']
 
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = (OwnerOrReadOnly,)
+    permission_classes = (OwnerOrReadOnlyTask,)
+
 
     @action(detail=True, methods=['get'])
     def detail_info(self, request, pk=None):
